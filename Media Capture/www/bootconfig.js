8293b1e036767dbdfb00055497ac7bfa4dc4a65a
Pat Patterson <ppatterson@salesforce.com>
2013-06-12 11:31:57 -0700

    Turned off debug, created Connected App record

--- Media Capture/www/bootconfig.js
+++ Media Capture/www/bootconfig.js
@@ -4,18 +4,18 @@
 
 // When debugMode is true, logToConsole() messages will be written to a
 // "debug console" section of the page.
-var debugMode = true;
+var debugMode = false;
 
 // The client ID value specified for your remote access object that defines
 // your application in Salesforce.
-var remoteAccessConsumerKey = "3MVG9Iu66FKeHhINkB1l7xt7kR8czFcCTUhgoA8Ol2Ltf1eYHOU4SqQRSEitYFDUpqRWcoQ2.dBv_a1Dyu5xa";
+var remoteAccessConsumerKey = "3MVG9y6x0357HleczgGPFxcVeCMoQknp2Vzy91UCTv421ang0S7DhLehgHMB0Tb1FYqoLmC9OHVCuxJAa4K11";
 
 // The redirect URI value specified for your remote access object that defines
 // your application in Salesforce.
-var oauthRedirectURI = "testsfdc:///mobilesdk/detect/oauth/done";
+var oauthRedirectURI = "mediacap:///mobilesdk/detect/oauth/done";
 
 // The authorization/access scope(s) you wish to define for your application.
-var oauthScopes = ["web","api"];
+var oauthScopes = ["api"];
 
 // The start data associated with the application.  Use SFHybridApp.LocalAppStartData for a "local"
 // Cordova-based application, and SFHybridApp.RemoteAppStartData for a Visualforce-based
